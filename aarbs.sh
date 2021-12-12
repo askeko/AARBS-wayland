1a77c76c43588d3ba14f2be36fc69f63db9374ee
Aske Klitgaard Ottesen <Askeklitgaard@gmail.com>
2021-12-12 20:52:17 +0100

    Update aarbs.sh

--- aarbs.sh
+++ aarbs.sh
@@ -169,6 +169,9 @@ preinstallmsg || error "User exited."
 
 ### The rest of the script requires no user input.
 
+# Refresh Arch keyrings.
+refreshkeys || error "Error automatically refreshing Arch keyring. Consider doing so manually."
+
 for x in curl ca-certificates base-devel git ntp zsh ; do
 	dialog --title "AARBS Installation" --infobox "Installing \`$x\` which is required to install and configure other programs." 5 70
 	installpkg "$x"
