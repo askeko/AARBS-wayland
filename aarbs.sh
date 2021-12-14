ebfa5961799bbfaa3f765efc638e8e39e1f8264e
Aske Klitgaard Ottesen <Askeklitgaard@gmail.com>
2021-12-14 23:27:50 +0100

    Update aarbs.sh

--- aarbs.sh
+++ aarbs.sh
@@ -13,7 +13,7 @@ while getopts ":a:r:b:p:h" o; do case "${o}" in
 	*) printf "Invalid option: -%s\\n" "$OPTARG" && exit 1 ;;
 esac done
 
-[ -z "$dotfilesrepo" ] && dotfilesrepo="https://github.com/askeko/absrice.git"
+[ -z "$dotfilesrepo" ] && dotfilesrepo="https://github.com/askeko/TEMP.git"
 [ -z "$progsfile" ] && progsfile="https://raw.githubusercontent.com/askeko/AARBS/master/progs.csv"
 [ -z "$aurhelper" ] && aurhelper="yay"
 [ -z "$repobranch" ] && repobranch="master"
